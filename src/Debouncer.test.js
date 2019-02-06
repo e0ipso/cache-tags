67a30bf3472fc4cab720d9c272fcaf608c2da3a6
Patrick Coffey <patrickcoffey48@gmail.com>
2019-02-06 17:17:16 -0600

    feat: expose inflight singleton as a Debouncer class prop, test debounce method (#11)
    * feat: expose inflight global as a Debouncer class prop, test
    
    * feat: improve Debouncer.inflight var docblock

--- src/Debouncer.test.js
+++ src/Debouncer.test.js
@@ -11,4 +11,24 @@ describe('Debouncer', () => {
       expect(e.message).toBe('Boooh!');
     });
   });
+
+  test('debounce behaves as expected', async () => {
+    expect.assertions(4);
+    const key = 'foo::[]';
+    const expected = Promise.resolve('Completed');
+
+    const backend = {
+      foo: jest.fn().mockReturnValue(expected),
+    };
+
+    const sut = new Debouncer(backend);
+    sut.inflight.get = jest.fn();
+    sut.inflight.set = jest.fn();
+    sut.inflight.delete = jest.fn();
+
+    expect(await sut.debounce('foo', [])).toBe('Completed');
+    expect(sut.inflight.get).toHaveBeenCalledWith(key);
+    expect(sut.inflight.set).toHaveBeenCalledWith(key, expected);
+    expect(sut.inflight.delete).toHaveBeenCalledWith(key);
+  });
 });
