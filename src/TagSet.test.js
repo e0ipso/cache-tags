2e541ce16a658bf274f48e0cd885da22a7cfcd7b
Elliott Foster <elliott@fourkitchens.com>
2019-08-13 12:20:48 -0500

    test(ttl): add unit tests for expire cases

--- src/TagSet.test.js
+++ src/TagSet.test.js
@@ -65,4 +65,22 @@ describe('TagSet', () => {
       expect(sut.debouncer.debounce).toHaveBeenCalledWith('get', 'whatever');
     });
   });
+
+  test('it can expire with seconds', async () => {
+    expect.assertions(1);
+    store.get = jest.fn().mockResolvedValue();
+    store.setnx = jest.fn().mockResolvedValue(0);
+    store.expire = jest.fn().mockResolvedValue();
+    await sut.initTag('whatever', 'EX', 1);
+    expect(store.expire).toHaveBeenCalledWith('whatever', 1);
+  });
+
+  test('it can expire with milliseconds', async () => {
+    expect.assertions(1);
+    store.get = jest.fn().mockResolvedValue();
+    store.setnx = jest.fn().mockResolvedValue(0);
+    store.pexpire = jest.fn().mockResolvedValue();
+    await sut.initTag('whatever', 'PX', 1);
+    expect(store.pexpire).toHaveBeenCalledWith('whatever', 1);
+  });
 });
